9e2a2f6b8a9cabaceb74e6c58d6422699a871a9f
Anka <ankakulczycka@gmail.com>
2018-06-21 10:13:37 +0100

    add clearFunction

--- app.js
+++ app.js
@@ -13,15 +13,25 @@ for (var i= 0; i < freeLanes; i++) {
 }
 
 morningButton.addEventListener("click", function() {
+clearLanes(lanes);
 showLanes(1,7)
 }); 
 
 noonButton.addEventListener("click", function(){
+clearLanes(lanes);    
 showLanes(5,3);
 });
 
 eveButton.addEventListener("click", function(){
+clearLanes(lanes);    
 showLanes(8,0);
 });
 
 //now lanes not clearing after clicking on a button
+//add a function clearing the lanes 
+
+function clearLanes(){
+     for(var i = 0; i < lanes.length; i++) {
+    lanes[i].style.backgroundColor = '#CCC';
+  }
+}
